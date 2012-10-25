eb825c28114e2342bdbe4780698d7b2e173fb62b
Markus Lanthaler <mark_lanthaler@gmx.net>
2012-10-25 17:18:53 +0200

    Consolidate view.xml and services.xml into services.xml

--- DependencyInjection/HydraExtension.php
+++ DependencyInjection/HydraExtension.php
@@ -24,6 +24,5 @@ class HydraExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
-        $loader->load('view.xml');
     }
 }
